d16369b16529ea9edac1fe6a716b6437fc5e85da
thundersparkf <devagastya0@gmail.com>
2021-04-10 18:25:45 +0530

    Terra cloud configured

--- terra/main.tf
+++ terra/main.tf
@@ -1,9 +1,16 @@
+
 terraform {
+  backend "remote" {
+    organization = "nesamani"
+
+    workspaces {
+      name = "gh-actions"
+    }
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "3.63.0"
     }
   }
-
 }

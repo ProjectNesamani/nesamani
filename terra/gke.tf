585d30d2fe323532caf73f7d20c42c07719c33b2
thundersparkf <devagastya0@gmail.com>
2021-04-11 13:25:43 +0530

    GKE file terraform update

--- terra/gke.tf
+++ terra/gke.tf
@@ -1,15 +1,15 @@
 variable "gke_username" {
-  default     = "nesamani_root"
+  default     = ""
   description = "gke username"
 }
 
 variable "gke_password" {
-  default     = "nesamani_root"
+  default     = ""
   description = "gke password"
 }
 
 variable "gke_num_nodes" {
-  default     = 1
+  default     = 2
   description = "number of gke nodes"
 }
 
@@ -36,11 +36,11 @@ resource "google_container_cluster" "primary" {
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name               = "${google_container_cluster.primary.name}-node-pool"
-  location           = var.region
-  cluster            = google_container_cluster.primary.name
-  node_count         = var.gke_num_nodes
-  initial_node_count = var.gke_num_nodes
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
@@ -52,10 +52,30 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-2"
+    machine_type = "n1-standard-1"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
   }
 }
+
+
+# # Kubernetes provider
+# # The Terraform Kubernetes Provider configuration below is used as a learning reference only.
+# # It references the variables and resources provisioned in this file.
+# # We recommend you put this in another file -- so you can have a more modular configuration.
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
+
+# provider "kubernetes" {
+#   load_config_file = "false"
+
+#   host     = google_container_cluster.primary.endpoint
+#   username = var.gke_username
+#   password = var.gke_password
+
+#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+#   client_key             = google_container_cluster.primary.master_auth.0.client_key
+#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+# }
